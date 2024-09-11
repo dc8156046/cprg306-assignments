563c728e168b3580488a742ba481d9f9a0c80fe0
Dan Chen <dan.chen01@edu.sait.ca>
2024-09-11 12:47:20 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ export default function Page() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="/week-2">Week 2</Link>
+      <Link href="/week-2">Week 2 Assignment</Link>
     </main>
   );
 }
