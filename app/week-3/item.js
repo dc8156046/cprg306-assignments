6d7ac94ced030b1eebc9d5aec4c5501c7b715631
Dan Chen <dan.chen01@edu.sait.ca>
2024-09-18 13:15:33 -0600

    update css

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,6 +1,6 @@
 export default function Item({ name, quantity, category }) {
   return (
-    <li className="p-2 m-4 bg-slate-900 max-w-sm">
+    <li className="p-2 m-4 bg-slate-300 max-w-sm">
       <h2 className="text-xl font-bold">{name}</h2>
       <div className="text-sm">
         Buy {quantity} in {category}
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -2,7 +2,7 @@ import ItemList from "./item-list";
 
 export default function Page() {
   return (
-    <main className="bg-slate-950">
+    <main className="bg-slate-100">
       <div>
         <h2 className="text-3xl font-bold m-2">Shopping List</h2>
         <ItemList />
