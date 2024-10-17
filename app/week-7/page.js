30c9f61e5f074edeb568905e0a58fed9429e8de6
Dan Chen <dan.chen01@edu.sait.ca>
2024-10-16 21:11:09 -0600

    add sort button

--- app/week-7/page.js
+++ app/week-7/page.js
@@ -9,9 +9,42 @@ export default function Page() {
   const handleAddItem = (item) => {
     setItems([...items, item]);
   };
+
+  const [activeButton, setActiveButton] = useState("");
+
+  const handleClick = (sortAttr) => {
+    const sortedItems = [...items].sort((a, b) => {
+      return a[sortAttr].localeCompare(b[sortAttr]);
+    });
+
+    setItems(sortedItems);
+    setActiveButton(sortAttr);
+  };
+
+  const getButtonClass = (sortAttr) => {
+    return activeButton === sortAttr
+      ? "bg-orange-500 p-1 m-2 w-28" // Active style
+      : "bg-orange-700 p-1 m-2 w-28"; // Inactive style
+  };
+
   return (
     <div className="flex flex-col items-center">
       <NewItem onAddItem={handleAddItem} />
+      <div>
+        <label for="sort">Sort by: </label>
+        <button
+          className={getButtonClass("name")}
+          onClick={() => handleClick("name")}
+        >
+          Name
+        </button>
+        <button
+          className={getButtonClass("category")}
+          onClick={() => handleClick("category")}
+        >
+          Category
+        </button>
+      </div>
       <ItemList items={items} />
     </div>
   );
