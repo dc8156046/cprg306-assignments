5fa96b62355292dc51daa0a8e01ac53d4e1c1ab7
Dan Chen <dan.chen01@edu.sait.ca>
2024-10-23 20:34:01 -0600

    week 8 assignment

---
+++ app/week-8/meal-ideas.js
@@ -0,0 +1,70 @@
+"use client";
+import { useState, useEffect } from "react";
+
+export default function MealIdeas({ itemName }) {
+  const [meals, setMeals] = useState([]);
+  const [ingredient, setIngredient] = useState(itemName);
+
+  const removeEmojis = (text) => {
+    return text
+      .replace(
+        /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
+        ""
+      )
+      .split(",")[0]
+      .trim();
+  };
+
+  const fetchMealIdeas = async (ingredient) => {
+    const sanitizedIngredient = removeEmojis(ingredient);
+    try {
+      const response = await fetch(
+        `https://www.themealdb.com/api/json/v1/1/filter.php?i=${sanitizedIngredient}`
+      );
+      const data = await response.json();
+      setMeals(data.meals || []);
+    } catch (error) {
+      console.error(error);
+      setMeals([]);
+    }
+  };
+
+  const loadMealIdeas = (ingredient) => {
+    fetchMealIdeas(ingredient);
+  };
+
+  useEffect(() => {
+    if (itemName) {
+      setIngredient(itemName);
+    }
+  }, [itemName]);
+
+  useEffect(() => {
+    if (ingredient) {
+      fetchMealIdeas(ingredient);
+    }
+  }, [ingredient]);
+
+  return (
+    <div>
+      <h3 class="text-xl font-bold">Meal Ideas</h3>
+      <div>
+        <p>
+          {meals.length > 0
+            ? `Here are some meal ideas using ${removeEmojis(ingredient)}:`
+            : `No meal ideas found for ${removeEmojis(ingredient)}`}
+        </p>
+        <ul>
+          {meals.map((meal) => (
+            <li
+              key={meal.idMeal}
+              class="p-2 m-1 bg-slate-300 max-w-sm hover:bg-orange-800 cursor-pointer"
+            >
+              {meal.strMeal}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
---
+++ app/week-8/page.js
@@ -0,0 +1,63 @@
+"use client";
+import { useState } from "react";
+import NewItem from "./new-item";
+import ItemList from "./item-list";
+import itemsData from "./items.json";
+import MealIdeas from "./meal-ideas";
+
+export default function Page() {
+  const [items, setItems] = useState(itemsData);
+  const [selectedItemName, setSelectedItemName] = useState("");
+
+  const handleItemSelect = (name) => {
+    setSelectedItemName(name);
+  };
+
+  const handleAddItem = (item) => {
+    setItems([...items, item]);
+  };
+
+  const [activeButton, setActiveButton] = useState("");
+
+  const handleClick = (sortAttr) => {
+    const sortedItems = [...items].sort((a, b) => {
+      return a[sortAttr].localeCompare(b[sortAttr]);
+    });
+
+    setItems(sortedItems);
+    setActiveButton(sortAttr);
+  };
+
+  const getButtonClass = (sortAttr) => {
+    return activeButton === sortAttr
+      ? "bg-orange-500 p-1 m-2 w-28" // Active style
+      : "bg-orange-700 p-1 m-2 w-28"; // Inactive style
+  };
+
+  return (
+    <div className="flex">
+      <div className="flex-1 max-w-sm m-2">
+        <NewItem onAddItem={handleAddItem} />
+        <div>
+          <label for="sort">Sort by: </label>
+          <button
+            className={getButtonClass("name")}
+            onClick={() => handleClick("name")}
+          >
+            Name
+          </button>
+          <button
+            className={getButtonClass("category")}
+            onClick={() => handleClick("category")}
+          >
+            Category
+          </button>
+        </div>
+        <ItemList items={items} onItemSelect={handleItemSelect} />
+      </div>
+      <div className="flex-1 max-w-sm m-2">
+        <MealIdeas itemName={selectedItemName} />
+      </div>
+    </div>
+  );
+}
